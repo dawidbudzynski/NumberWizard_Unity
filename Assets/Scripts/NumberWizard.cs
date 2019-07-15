572d68f8b32687801a3e494d8edee25f3812127f
Dawid <budzynski.dawid@gmail.com>
2019-07-15 21:43:21 +0200

    UI version done

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max_num;
+    [SerializeField] int min_num;
+    [SerializeField] int guess;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    void Start()
+    {
+        if (guessText)
+        {
+            NextGuess();
+        }
+    }
+
+    public void OnPressHigher()
+    {
+        min_num = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max_num = guess - 1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min_num, max_num + 1);
+        guessText.text = "Is your number: " + guess.ToString() + "?";
+    }
+}
